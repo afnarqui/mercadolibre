85a6fbd61d1df913e1f157031b5a7f87ba8ef089
Andrés Naranjo <afnarqui9@gmail.com>
2021-06-10 16:21:05 -0500

    agrego el favicon

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'src/index.html'
+            template: 'src/index.html',
+            favicon: "./src/favicon.ico"
         })
     ],
     module: {
